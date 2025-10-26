2455df6723772bf371fc908899b6901d9771fb37
Ayush-B99 <ayush.beekum@gmail.com>
2025-10-26 22:54:08 +0200

    cpp files from diya added

--- src/Sprout.cpp
+++ src/Sprout.cpp
@@ -1,6 +1,27 @@
 #include "../include/Sprout.h"
+#include "../include/Plant.h"
+#include "../include/Mature.h"
 
 void Sprout::grow(Plant* plant) {
-	// TODO - implement Sprout::grow
-	throw "Not yet implemented";
+	if (plant->getCurrentCycleCount() >= plant->getSproutCyclesNeeded()) {
+		plant->setGrowthState(new Mature());
+		plant->resetCycleCount();
+		plant->notify();
+	}
 }
+
+vector<string> Sprout::getRequiredCare() {
+	return {"Water", "sunlight", "fertilizer"};
+
+}
+
+string Sprout::getStatusMessage(Plant* plant) {
+	int current = plant->getCurrentCycleCount();
+	int needed = plant->getSproutCyclesNeeded();
+	return plant->getSpecies() + " (Sprout): " + to_string(current) + "/" + to_string(needed) + " care cycles completed";
+}
+
+bool Sprout::isMature() {
+	return false;
+}
+
